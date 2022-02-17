a41b46f6f05243e46019a1750d36c5c76e139d57
AmadorZcv <lucasamadorzcv@gmail.com>
2022-02-17 06:22:52 -0300

    feat: added public functions according to specs

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -4,15 +4,70 @@ defmodule ExBanking do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBanking.hello()
-      :world
+  Creates user
 
   """
-  def hello do
-    :world
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(_user) do
+    {:error, :wrong_arguments}
+  end
+
+  @doc """
+  Deposit money to a user in a currency
+
+  """
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(_user, _amount, _currency) do
+    {:error, :wrong_arguments}
+  end
+
+  @doc """
+  Withdraw money from a user in a currency
+
+  """
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :user_does_not_exist
+             | :not_enough_money
+             | :too_many_requests_to_user}
+  def withdraw(_user, _amount, _currency) do
+    {:error, :wrong_arguments}
+  end
+
+  @doc """
+  Get user balance in a currency
+
+  """
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def get_balance(_user, _currency) do
+    {:error, :wrong_arguments}
+  end
+
+  @doc """
+  Send money from a user to another in a currency
+
+  """
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) ::
+          {:ok, from_user_balance :: number, to_user_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :not_enough_money
+             | :sender_does_not_exist
+             | :receiver_does_not_exist
+             | :too_many_requests_to_sender
+             | :too_many_requests_to_receiver}
+  def send(_from_user, _to_user, _amount, _currency) do
+    {:error, :wrong_arguments}
   end
 end

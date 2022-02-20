3eab6c5f809d2480d919796fd5958be1b243ac1a
AmadorZcv <lucasamadorzcv@gmail.com>
2022-02-20 07:19:59 -0300

    refactor: better error handling

--- lib/ex_banking/bank.ex
+++ lib/ex_banking/bank.ex
@@ -30,44 +30,38 @@ defmodule ExBanking.Bank do
   end
 
   def send(from_user, to_user, amount, currency) do
-    with {:ok, _from_user} <- User.get(from_user) |> handle_from_user_errors,
-         {:ok, _from_user} <- User.get(to_user) |> handle_to_user_errors,
-         {:ok, from_user_balance} <-
-           User.withdraw(from_user, amount, currency) |> handle_from_user_errors,
-         {:ok, to_user_balance} <-
-           User.deposit(to_user, amount, currency) |> handle_to_user_errors do
+    with {:sender, {:ok, _from_user}} <- {:sender, User.get(from_user)},
+         {:receiver, {:ok, _from_user}} <- {:receiver, User.get(to_user)},
+         {:sender, {:ok, from_user_balance}} <-
+           {:sender, User.withdraw(from_user, amount, currency)},
+         {:receiver, {:ok, to_user_balance}} <-
+           {:receiver, User.deposit(to_user, amount, currency)} do
       {:ok, from_user_balance, to_user_balance}
     else
-      {:error, :too_many_requests_to_receiver} ->
+      {:receiver, {:error, :too_many_requests_to_user}} ->
         User.deposit(from_user, amount, currency, :fast)
         {:error, :too_many_requests_to_receiver}
 
       error ->
-        error
+        handle_errors(error)
     end
   end
 
-  defp handle_from_user_errors({:error, :user_does_not_exist}),
+  defp handle_errors({:sender, {:error, :user_does_not_exist}}),
     do: {:error, :sender_does_not_exist}
 
-  defp handle_from_user_errors({:error, :not_enough_money}),
-    do: {:error, :not_enough_money}
-
-  defp handle_from_user_errors({:error, :too_many_requests_to_user}),
+  defp handle_errors({:sender, {:error, :too_many_requests_to_user}}),
     do: {:error, :too_many_requests_to_sender}
 
-  defp handle_from_user_errors({:ok, state}),
-    do: {:ok, state}
-
-  defp handle_to_user_errors({:error, :user_does_not_exist}),
+  defp handle_errors({:receiver, {:error, :user_does_not_exist}}),
     do: {:error, :receiver_does_not_exist}
 
-  defp handle_to_user_errors({:error, :not_enough_money}),
-    do: {:error, :not_enough_money}
-
-  defp handle_to_user_errors({:error, :too_many_requests_to_user}),
+  defp handle_errors({:receiver, {:error, :too_many_requests_to_user}}),
     do: {:error, :too_many_requests_to_receiver}
 
-  defp handle_to_user_errors({:ok, state}),
+  defp handle_errors({_, {:ok, state}}),
     do: {:ok, state}
+
+  defp handle_errors({_, {:error, :not_enough_money}}),
+    do: {:error, :not_enough_money}
 end
